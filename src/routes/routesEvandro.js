2064f02150991a8f5c0e7d006f03498c468a6afb
EvandroOr <evandroetecemail@gmail.com>
2025-05-19 08:50:15 -0300

    Atualização de nome de pastas

--- src/routes/routesEvandro.js
+++ src/routes/routesEvandro.js
@@ -6,6 +6,6 @@ const infsJogoController = require('../controllers/infsJogo');
 router.get('/infsJogo', infsJogoController.listarInfsJogo); 
 router.post('/infsJogo', infsJogoController.cadastrarInfsJogo); 
 router.patch('/infsJogo/:id', infsJogoController.editarInfsJogo); 
-router.delete('/infsJogo/:id', infsJogoController.apagarInfsJogo); 
+router.delete('/infsJogo/:id', infsJogoController.apagarInfsJogo);
 
 module.exports = router;
